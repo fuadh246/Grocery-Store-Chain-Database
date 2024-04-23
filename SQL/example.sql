b3931c60645b8d38f27d0a675a6ecb16492bf5d2
Fuad Hassan <93026516+fuadh246@users.noreply.github.com>
2024-04-23 03:37:06 +0000

    Removed some insert and other stuff

--- SQL/example.sql
+++ SQL/example.sql
@@ -1,14 +1,20 @@
-select o.startdate,o.orderid,o.TIMECOMPLETED
+-- 1. gets the totalCost per order
+SELECT ORDERS_GS.CustomerID,
+    ORDERS_GS.OrderID,
+    CUSTOMERS_GS.FirstName,
+    CUSTOMERS_GS.LastName,
+    SUM(ITEMS_GS.Price) AS TotalItemCost
+FROM ORDERS_GS
+    JOIN CUSTOMERS_GS ON ORDERS_GS.CustomerID = CUSTOMERS_GS.CustomerID
+    JOIN ORDERS_DETAILS_GS ON ORDERS_GS.OrderID = ORDERS_DETAILS_GS.OrderID
+    JOIN ITEMS_GS ON ORDERS_DETAILS_GS.ItemID = ITEMS_GS.ItemID
+GROUP BY ORDERS_GS.CustomerID,
+    ORDERS_GS.OrderID,
+    CUSTOMERS_GS.FirstName,
+    CUSTOMERS_GS.LastName;
+-- 2. Shows the Order and its start and end time
+SELECT o.orderid,
+    o.startdate,
+    o.TIMECOMPLETED
 from ORDERS_GS o
-inner join CUSTOMERS_GS c
-    on o.CustomerID = c.CustomerID
-
-
-select 
-    o.orderid,
-    i.price
-from orders_gs o
-inner join orders_details_gs od
-    on o.orderid = od.orderid
-inner join items_gs i
-    on od.itemid = i.itemid
+    inner join CUSTOMERS_GS c on o.CustomerID = c.CustomerID;
